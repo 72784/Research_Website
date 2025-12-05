2aa7691bc6b3730246203697a95e12ca6a117bea
Parth Shiroya <parthshiroya2001@gmail.com>
2025-12-05 00:09:24 -0800

    Improved Publications (Added Images) & Footer

--- script.js
+++ script.js
@@ -9,20 +9,27 @@ fetch("./data/papers.json")
       card.classList.add("pub-card");
 
       card.innerHTML = `
-        <h3>${paper.title}</h3>
+  <div class="pub-card-inner">
+      <img class="pub-image" src="${paper.image || 'data/icons/paper_default.png'}" alt="Paper Image">
 
-        <p class="pub-meta">
-            <strong>Authors:</strong> ${paper.authors}<br>
-            <strong>Venue:</strong> ${paper.venue}<br>
-            <strong>Year:</strong> ${paper.year}
-        </p>
+      <div class="pub-content">
+          <h3>${paper.title}</h3>
 
-        <p class="pub-abstract">${paper.abstract}</p>
+          <p class="pub-meta">
+              <strong>Authors:</strong> ${paper.authors}<br>
+              <strong>Venue:</strong> ${paper.venue}<br>
+              <strong>Year:</strong> ${paper.year}
+          </p>
 
-        <a href="${paper.pdf_link}" class="pub-link" target="_blank">
-            View PDF
-        </a>
-      `;
+          <p class="pub-abstract">${paper.abstract}</p>
+
+          <a href="${paper.pdf_link}" class="pub-link" target="_blank">
+              View PDF
+          </a>
+      </div>
+  </div>
+`;
+
 
       container.appendChild(card);
     });
